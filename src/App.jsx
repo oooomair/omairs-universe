1f8865a7879de5b4d23e7f175fec4a3abf428cc9
oooomair <omairsalam123@gmail.com>
2022-04-23 02:54:17 +0400

    background check

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,7 @@ import './app.scss';
 function App() {
   return (
     <div className="App">
-      <div className="bro">
-        
-      </div>
+      {/* <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad culpa ratione ea tempore inventore ipsam accusamus suscipit, similique obcaecati sed quod voluptatibus. Obcaecati placeat neque sed adipisci nisi atque, totam provident, quod sit pariatur beatae dolorum sunt eum non quasi facilis voluptatum aliquam minus accusamus? Et consequatur omnis odio quo dolorum voluptates optio neque, architecto repudiandae consequuntur excepturi dignissimos possimus quae ad aperiam rem nihil aliquam, corrupti animi id fuga alias ullam! Numquam voluptate sit exercitationem? Necessitatibus recusandae ullam laborum magni ducimus non. Quidem ipsa architecto assumenda consectetur odit blanditiis maiores id explicabo? Unde suscipit corrupti ipsam dolor quibusdam harum. </p> */}
     </div>
   );
 }
